5c4e82572113bb3f1d368c1c72d9acb27230881f
Luna Razzaghipour <luna@xoria.org>
2024-10-02 12:42:35 +1000

    Initialize repository

---
+++ Source/Entry Point.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+#include <stdio.h>
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+typedef ptrdiff_t smm;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef size_t umm;
+
+s32
+main(void)
+{
+	printf("hello world\n");
+}
