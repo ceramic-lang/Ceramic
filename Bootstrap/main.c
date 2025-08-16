062f506100a479c04fafe0751637c22bd76cbaff
Luna Razzaghipour <luna@xoria.org>
2025-08-17 00:36:15 +1000

    Add simple binary expression evaluator

--- Bootstrap/main.c
+++ Bootstrap/main.c
@@ -4,18 +4,28 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-static void cm_assert(bool b) {
-	if (!b) __builtin_trap();
+_Noreturn static void unreachable(void) {
+	__builtin_trap();
+}
+
+static void assert(bool b) {
+	if (!b) unreachable();
 }
 
 enum token_kind {
 	token_kind_name,
 	token_kind_number,
+	token_kind_plus,
+	token_kind_hyphen,
+	token_kind_eof,
 };
 
 static char *const token_kind_strings[] = {
         [token_kind_name] = "name",
         [token_kind_number] = "number",
+        [token_kind_plus] = "“+”",
+        [token_kind_hyphen] = "“-”",
+        [token_kind_eof] = "end of file",
 };
 
 struct token {
@@ -35,28 +45,6 @@ static bool is_alpha(char c) {
 
 static bool is_digit(char c) {
 	return c >= '0' && c <= '9';
-}
-
-static char *tokens_print(struct tokens tokens) {
-	size_t capacity = 128 * 1024;
-	char *result = malloc(capacity);
-	char *s = result;
-	size_t remaining = capacity;
-
-	struct token *end = tokens.ptr + tokens.count;
-	for (struct token *token = tokens.ptr; token != end; token++) {
-		char *token_kind_string = token_kind_strings[token->kind];
-		int return_value =
-		        snprintf(s, remaining, "%s:%zu \"%s\"\n", token_kind_string, token->line, token->string);
-		if (return_value < 0) break;
-
-		size_t nprinted = (size_t)return_value;
-		if (nprinted + 1 > remaining) break;
-		remaining -= nprinted;
-		s += nprinted;
-	}
-
-	return result;
 }
 
 static struct tokens lex(char *s) {
@@ -85,6 +73,20 @@ static struct tokens lex(char *s) {
 			do {
 				s++;
 			} while (is_digit(*s));
+		} else {
+			switch (*s) {
+				case '+':
+					token.kind = token_kind_plus;
+					break;
+				case '-':
+					token.kind = token_kind_hyphen;
+					break;
+				default:
+					printf("%zu: invalid token “%c”\n", line, *s);
+					exit(1);
+					break;
+			}
+			s++;
 		}
 
 		size_t len = (size_t)(s - start);
@@ -92,7 +94,7 @@ static struct tokens lex(char *s) {
 		token.string = memcpy(token_string, start, len);
 		token.line = line;
 
-		cm_assert(count <= capacity);
+		assert(count <= capacity);
 		if (count == capacity) {
 			capacity *= 2;
 			tokens = realloc(tokens, sizeof(struct token) * capacity);
@@ -106,6 +108,100 @@ static struct tokens lex(char *s) {
 	return result;
 }
 
+struct expr {
+	uint64_t lhs;
+	uint64_t rhs;
+	enum token_kind op;
+};
+
+struct parser {
+	struct token *token;
+	size_t remaining_tokens;
+	struct expr expr;
+};
+
+static uint64_t parse_number(char *s) {
+	uint64_t result = 0;
+	for (; *s; s++) {
+		if (!is_digit(*s)) break;
+		result *= 10;
+		result += (uint64_t)(*s - '0');
+	}
+	return result;
+}
+
+static enum token_kind parser_current(struct parser *p) {
+	return p->token->kind;
+}
+
+static struct token *parser_bump(struct parser *p, enum token_kind kind) {
+	assert(parser_current(p) == kind);
+	struct token *token = p->token;
+	p->remaining_tokens--;
+	if (p->remaining_tokens == 0) {
+		struct token *eof_token = calloc(1, sizeof(struct token));
+		eof_token->kind = token_kind_eof;
+		eof_token->line = token->line;
+		p->token = eof_token;
+	} else {
+		p->token++;
+	}
+	return token;
+}
+
+static struct token *parser_expect(struct parser *p, enum token_kind kind) {
+	assert(kind != token_kind_eof);
+	if (parser_current(p) != kind) {
+		printf("%zu: expected %s, found %s\n", p->token->line, token_kind_strings[kind],
+		        token_kind_strings[parser_current(p)]);
+		exit(1);
+	}
+	return parser_bump(p, kind);
+}
+
+static void parse_expr(struct parser *p) {
+	struct token *lhs = parser_expect(p, token_kind_number);
+
+	struct token *op = 0;
+	switch (parser_current(p)) {
+		case token_kind_plus:
+		case token_kind_hyphen:
+			op = parser_bump(p, parser_current(p));
+			break;
+
+		default:
+			printf("%zu: expected operator\n", p->token->line);
+			exit(1);
+			break;
+	}
+
+	struct token *rhs = parser_expect(p, token_kind_number);
+
+	p->expr.lhs = parse_number(lhs->string);
+	p->expr.rhs = parse_number(rhs->string);
+	p->expr.op = op->kind;
+}
+
+static struct expr parse(char *s) {
+	struct tokens tokens = lex(s);
+	struct parser p = {0};
+	p.token = tokens.ptr;
+	p.remaining_tokens = tokens.count;
+	parse_expr(&p);
+	return p.expr;
+}
+
+static uint64_t eval(struct expr expr) {
+	switch (expr.op) {
+		case token_kind_plus:
+			return expr.lhs + expr.rhs;
+		case token_kind_hyphen:
+			return expr.lhs - expr.rhs;
+		default:
+			unreachable();
+	}
+}
+
 int main(void) {
 	static const size_t buffer_size = 1024;
 	char buffer[buffer_size] = {0};
@@ -117,6 +213,6 @@ int main(void) {
 		buffer[n++] = c;
 	}
 
-	struct tokens tokens = lex(buffer);
-	printf("%s", tokens_print(tokens));
+	struct expr expr = parse(buffer);
+	printf("%llu\n", eval(expr));
 }
